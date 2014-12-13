cbe386b342fde7ee2f97338ed372edb843c290c6
Aram Kocharyan <akarmenia@gmail.com>
2014-12-13 11:35:27 +1100

    Meteor package version bump.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'aramk:q',
   summary: "A tool for making and composing asynchronous promises in JavaScript",
-  version: "1.0.1",
+  version: "1.0.1_1",
   git: "https://github.com/aramk/meteor-q.git"
 });
 
