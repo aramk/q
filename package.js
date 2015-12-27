80e746f7d15fd922265838e8b2373ee5f8dba5f7
Aram Kocharyan <akarmenia@gmail.com>
2015-12-27 17:09:22 +1100

    Fix URL

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'aramk:q',
   summary: "A tool for making and composing asynchronous promises in JavaScript",
   version: "1.0.1_1",
-  git: "https://github.com/aramk/meteor-q.git"
+  git: "https://github.com/aramk/q.git"
 });
 
 Package.on_use(function (api) {
